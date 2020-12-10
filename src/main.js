41c525481b1c499b0b109de47579985eac322a97
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2020-12-10 17:35:44 +0100

    jquery added

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'jquery/dist/jquery.min.js'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/style.css'
-// import '@/assets/all.min.css'
-
-
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
