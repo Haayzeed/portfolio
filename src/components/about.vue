ced709974d1d6acd07939ca0dd1f155452b1235e
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2020-12-12 13:28:46 +0100

    updated informtion

--- src/components/about.vue
+++ src/components/about.vue
@@ -7,10 +7,11 @@
 					<h3><span class="mr-2">01. </span>About Me</h3>
 				</div>
 				<div class="col-md-7 about-me">
-					<p>Hi there! I am Dosunmu Afeez. I am a frontend developer and I live in Nigeria. I have always been in awe of the web.I enjoy crafting amazing digital experiences. I have a knack for creating user friendly, aesthetically pleasing and functional web products, ranging from websites to web apps.
+					<p>Hi there! I am Dosunmu Afeez. I am a frontend developer and I live in Nigeria. I have always been in awe of the web. I enjoy crafting amazing digital experiences. I have a knack for creating user friendly, aesthetically pleasing and functional web products, ranging from websites to web apps.
 
 					</p>
-					<p>I am a computer science major in my finals. I currently am a member of Code Clan Nigeria, where I enjoy working on meaningful projects on a daily basis. I have had the privilege of collaborating with others on small to medium sized projects, thereby building my skills. Here are some of the technologies I have been working with lately:</p>
+					<!-- I am a computer science major in my finals. I currently am a member of Code Clan Nigeria, where I enjoy working on meaningful projects on a daily basis. -->
+					<p> I have had the privilege of collaborating with others on small to medium sized projects, thereby building my skills. Here are some of the technologies I have been working with lately:</p>
 					<p>Here are a few technologies I've been working with recently:
 						<ul class="skill-list float-left ml-5 mr-5 fa-ul">
 							<li><span class="fa-li"><i class="fas fa-caret-right"></i></span>HTML & (S)CSS</li>
