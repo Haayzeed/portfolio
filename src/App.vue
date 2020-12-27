0a4e8c6dbe4684f5f69efbc88eb7d3dbfacadc55
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2020-12-27 04:52:29 +0100

    animation added

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
       timeline.from(".logo", {opacity: 0,  ease: "back", delay: 0.8})
 
       .from(".menu-animation li", {opacity: 0, ease: "back", stagger: 0.25}, 0)
-      .from(".fa-bars", {opacity: 0, ease: "back", x: 12 }, 0)
+      .from(".fa-bars", {opacity: 0, ease: "back", x: 12, delay: 0 })
       .from(".description .hi", {opacity: 0, ease: "sine", delay: 0, y: -10})
       .from(".description .name", {opacity: 0, ease: "sine",  y: -10, delay: 0})
       .from(".description .name-subtitle", {opacity: 0, ease: "back", y: 12, delay: 0 })
